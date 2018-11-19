c74d475f35bc3409e276d99d0957e740e3df98a7
zhemin <zhemin@zhemins-MacBook-Pro.local>
2018-11-18 17:56:15 -0800

    modify the web page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    hostname: 'http://linksmartsensing.us-east-2.elasticbeanstalk.com'
+  });
 });
 
 module.exports = router;
