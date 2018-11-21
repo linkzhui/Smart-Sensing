d16a064dab4850abaacbf8a64582935d8bc3c010
zhemin <zhemin@zhemins-MBP.attlocal.net>
2018-11-21 14:30:59 -0800

    fix

--- routes/data.js
+++ routes/data.js
@@ -8,34 +8,37 @@ AWS.config.update({
 });
 
 // Create DynamoDB service object
-//var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 var table = "DEVICE_LIST";
-var docClient = new AWS.DynamoDB.DocumentClient();
+// var docClient = new AWS.DynamoDB.DocumentClient();
 /* GET home page. */
 router.post('/sensor', function(req, res, next) {
     var device_id = req.query.device;
     var smoke = req.query.smoke;
     var longitude = req.query.longitude;
     var latitude = req.query.latitude;
-    // var params = {
-    //     TableName:table,
-    //     Item:{
-    //         "DEVICE_ID": device_id,
-    //         "smoke": smoke,
-    //         "longitude" : longitude,
-    //         "latitude" : latitude
-    //     }
-    // };
-    // console.log("Adding a new item...");
-    // docClient.put(params, function(err, data) {
-    //     if (err) {
-    //         res.send("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    //         console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    //     } else {
-    //         res.send("successful");
-    //         console.log("Added item:", JSON.stringify(data, null, 2));
-    //     }
-    // });
+    var params = {
+        TableName:table,
+        Item:{
+            "DEVICE_ID": device_id,
+            "smoke": smoke,
+            "longitude" : longitude,
+            "latitude" : latitude
+        }
+    };
+    console.log("Adding a new item...");
+    ddb.putItem({
+        'TableName':table,
+        'item':params
+    }, function(err, data) {
+        if (err) {
+            res.send("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            res.send("successful");
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
     res.send('smoke level: ' + req.query.smoke + ' \n' + 'longitude: ' + req.query.longitude + ' \n' + 'latitude: ' + req.query.latitude);
 });
 
